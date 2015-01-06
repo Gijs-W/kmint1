8ef6678c71709436a2696444deb427dea59b46c2
basterbogt <bas@basterbogt.com>
2015-01-06 17:28:58 +0100

    [BugFix] Un-updated position fixed

--- SDLFramework/SDLFramework/Gun.cpp
+++ SDLFramework/SDLFramework/Gun.cpp
@@ -7,7 +7,7 @@ Gun::Gun()
 }
 
 void Gun::Update(Instance* instance, float deltatime){
-
+	Move(0.0f);
 }
 
 Gun::~Gun()
--- SDLFramework/SDLFramework/Pill.cpp
+++ SDLFramework/SDLFramework/Pill.cpp
@@ -7,7 +7,7 @@ Pill::Pill()
 }
 
 void Pill::Update(Instance* instance, float deltatime){
-
+	Move(0.0f);
 }
 
 Pill::~Pill()
